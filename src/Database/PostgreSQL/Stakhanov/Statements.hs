8bfec9bf69b0ffcfcac67299244f7244aa9e7e36
Michel Boucey <michel.boucey@gmail.com>
2025-12-22 20:15:23 +0100

    Add getAllMetrics statement

--- src/Database/PostgreSQL/Stakhanov/Statements.hs
+++ src/Database/PostgreSQL/Stakhanov/Statements.hs
@@ -78,6 +78,22 @@ getMetrics =
             D.column (D.nonNullable D.timestamptz) <*>
             D.column (D.nonNullable D.int8)
 
+getAllMetrics :: Statement () (T.Text, Int64, Int32, Int32, Int64, UTCTime, Int64)
+getAllMetrics =
+  Statement sql E.noParams decoder True
+    where
+      sql = "select queue_name,queue_length,newest_msg_age_sec,oldest_msg_age_sec,total_messages,scrape_time,queue_visible_length from pgmq.metrics_all()"
+      decoder =
+        D.singleRow $
+          (,,,,,,) <$>
+            D.column (D.nonNullable D.text) <*>
+            D.column (D.nonNullable D.int8) <*>
+            D.column (D.nonNullable D.int4) <*>
+            D.column (D.nonNullable D.int4) <*>
+            D.column (D.nonNullable D.int8) <*>
+            D.column (D.nonNullable D.timestamptz) <*>
+            D.column (D.nonNullable D.int8)
+
 archiveMessage :: Statement (T.Text,Int64) Bool
 archiveMessage = [TH.singletonStatement|select pgmq.archive($1::text,$2::int8)::bool|]
 
