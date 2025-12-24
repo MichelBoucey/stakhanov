cfb51ccd6e2ab3b6a78c538247ff8b42a0c06cff
Michel Boucey <michel.boucey@gmail.com>
2025-12-24 16:59:23 +0100

    Add queue metric getters

--- src/Database/PostgreSQL/Stakhanov/Metrics.hs
+++ src/Database/PostgreSQL/Stakhanov/Metrics.hs
@@ -1,10 +1,22 @@
 module Database.PostgreSQL.Stakhanov.Metrics
  (
+
+ -- * Get queue(s) metrics
    metrics
  , allMetrics
 
+ -- * Queue metric getters
+ , getQueueLength
+ , getNewestMsgAge
+ , getOldestMsgAge
+ , getScrapeTime
+ , getTotalMessages
+ , getQueueVisibleLength
+
  ) where
 
+import           Data.Int
+import           Data.Time
 import qualified Data.Vector                              as V
 import           Database.PostgreSQL.Stakhanov.Internal
 import           Database.PostgreSQL.Stakhanov.Statements
@@ -32,4 +44,34 @@ allMetrics
   -> IO (Either S.SessionError (V.Vector Queue))
 allMetrics c =
   S.run (S.statement () getAllMetrics) c >>= \e -> pure $ (tupleToQueueWithMetrics <$>) <$> e
+
+-- | Number of messages currently in the queue.
+getQueueLength :: Queue -> Maybe Int64
+getQueueLength (Queue _ (Just Metrics { .. })) = Just queueLength
+getQueueLength (Queue _ Nothing)               = Nothing
+
+-- | Age of the newest message in the queue, in seconds.
+getNewestMsgAge :: Queue -> Maybe Int32
+getNewestMsgAge (Queue _ (Just Metrics{..})) = newestMsgAge
+getNewestMsgAge (Queue _ Nothing)            = Nothing
+
+-- | Age of the oldest message in the queue, in seconds.
+getOldestMsgAge :: Queue -> Maybe Int32
+getOldestMsgAge (Queue _ (Just Metrics{..})) = oldestMsgAge
+getOldestMsgAge (Queue _ Nothing)            = Nothing
+
+-- | Total number of messages that have passed through the queue over all time.
+getTotalMessages :: Queue -> Maybe Int64
+getTotalMessages (Queue _ (Just Metrics{..})) = Just totalMessages
+getTotalMessages (Queue _ Nothing)            = Nothing
+
+-- | The current timestamp
+getScrapeTime :: Queue -> Maybe UTCTime
+getScrapeTime (Queue _ (Just Metrics{..})) = Just scrapeTime
+getScrapeTime (Queue _ Nothing)            = Nothing
+
+-- | Number of messages currently visible (vt <= now).
+getQueueVisibleLength :: Queue -> Maybe Int64
+getQueueVisibleLength (Queue _ (Just Metrics{..})) = Just queueVisibleLength
+getQueueVisibleLength (Queue _ Nothing)            = Nothing
 
